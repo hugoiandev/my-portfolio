7a3c2900750020c091a5cb20e3c0a9dbe40ff2bf
Hugo Ian <ahugaao@gmail.com>
2021-11-26 06:00:36 -0300

    add feature in home

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -8,9 +8,10 @@ const Home = () => {
   const titleTwo = React.useRef()
   const titleThree = React.useRef()
   const buttonSobre = React.createRef()
+  const techContent = React.useRef()
   
   React.useEffect(() => {
-
+    // Title animation
     const animeTitle = () => {
       gsap.to(titleOne.current, {
         opacity: 1,
@@ -37,28 +38,57 @@ const Home = () => {
         duration: .7
       })
     }
+
+    // Tech animation
+    const animeTech = () => {
+      const width = techContent.current.offsetWidth
+      gsap.fromTo(techContent.current, {
+        x: width,
+        duration: 20,
+        ease: 'linear',
+        repeat: -1
+      }, {
+        x: -width,
+        duration: 20,
+        ease: 'linear',
+        repeat: -1
+        
+      })
+    }
+
+    animeTech()
     animeTitle()
     
   },[buttonSobre])
 
 
   return (
-    <section className={styles.home}>
-      <div>
-        <h1 className={styles.title}>
-          <div>
-            <span ref={titleOne}>OLÁ</span>
-          </div>
-          <div>
-            <span ref={titleTwo}>SEJA BEM VINDO</span>
-          </div>
-          <div>
-            <span ref={titleThree}>SOU DEV FRONT-END</span>
-          </div>
-        </h1>
-      </div>
-      <Button ref={buttonSobre} linkTo='/sobre' text='Sobre' />
-    </section>
+    <>
+      <section className={styles.home}>
+        <div className={styles.containerTitle}>
+          <h1 className={styles.title}>
+            <div>
+              <span ref={titleOne}>OLÁ</span>
+            </div>
+            <div>
+              <span ref={titleTwo}>SEJA BEM VINDO</span>
+            </div>
+            <div>
+              <span ref={titleThree}>SOU DEV FRONT-END</span>
+            </div>
+          </h1>
+        </div>
+        <Button ref={buttonSobre} linkTo='/sobre' text='Sobre' />
+        <div ref={techContent} className={styles.technologies}>
+          <img src="icon-node.svg" alt="" />
+          <img src="icon-javascript.svg" alt="" />
+          <img src="icon-react-native.svg" alt="" />
+          <img src="icon-sass.svg" alt="" />
+          <img src="icon-github.svg" alt="" />
+          <img src="icon-git.svg" alt="" />
+        </div>
+      </section>
+    </>
   )
 }
 
