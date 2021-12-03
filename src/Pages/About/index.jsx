a25595feb3c7bc7f7d96d40c343dab426b697a4a
Hugo Ian <ahugaao@gmail.com>
2021-12-03 00:43:41 -0300

    init development page about

--- src/Pages/About/index.jsx
+++ src/Pages/About/index.jsx
@@ -10,8 +10,13 @@ const About = () => {
       <Container>
         <section className={styles.about}>
           <Title text='Sobre' />
-          <div className={styles.textAbout}>
-            <p>Olá, meu nome é Hugo, tenho 23 anos e atualmente moro em Hortolândia/SP, gosto bastante de ouvir músicas e programar, desde mais jovem eu já era interessado pela tecnologia, mas ingressei na área de desenvolvimento no final do ano de 2019 e foi paixão de primeira vista, atualmente tenho focado meus estudos no desenvolvimento web onde tenho mais familiariadade, tenho conhecimento em ReactJS, React Native, NextJS, Sass, Styled components e o basico de Api's com NodeJS</p>
+          <div className={styles.aboutContainer}>
+            <article className={styles.textAbout}>
+              <p>Olá, meu nome é Hugo, tenho 23 anos e atualmente moro em Hortolândia/SP, gosto bastante de ouvir músicas e programar, desde mais jovem eu já era interessado pela tecnologia, mas ingressei na área de desenvolvimento no final do ano de 2019 e foi paixão de primeira vista, atualmente tenho focado meus estudos no desenvolvimento web onde tenho mais familiariadade, tenho conhecimento em ReactJS, React Native, NextJS, Sass, Styled components e o basico de Api's com NodeJS</p>
+            </article>
+            <div className={styles.imgAbout}>
+              <img src="img-about.jpg" alt="Foto Hugo" />
+            </div>
           </div>
         </section>
       </Container>
