720f36d1a00c4f8f63b628f37c6f1e9def2ce768
Hugo Ian <ahugaao@gmail.com>
2021-12-06 04:07:14 -0300

    edit page contact

--- src/Pages/Contact/index.jsx
+++ src/Pages/Contact/index.jsx
@@ -10,6 +10,17 @@ const Contact = () => {
       <Container>
         <section className={styles.contact}>
           <Title text='Contato' />
+          <div className={styles.containerContact}>
+            <div className={styles.email}>
+              <span>MANDE UM EMAIL</span>
+              <span>
+                <a href='mailto:hugofrontdev@gmail.com'>hugofrontdev@gmail.com</a>
+              </span>
+            </div>
+            <div className={styles.redes}>
+              <span>REDES SOCIAIS</span>
+            </div>
+          </div>
         </section>
       </Container>
       <Preload text='Contato' />
