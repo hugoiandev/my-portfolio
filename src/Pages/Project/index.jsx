2528fc0e52d1b1b8574835e4fdcdc9e055beb935
Hugo Ian <ahugaao@gmail.com>
2021-12-07 03:33:15 -0300

    remove arrow icon

--- src/Pages/Project/index.jsx
+++ src/Pages/Project/index.jsx
@@ -4,44 +4,22 @@ import Preload from '../../Components/Preload'
 import ProjectItem from '../../Components/ProjectItem'
 import Title from '../../Components/Title'
 import styles from './Project.module.scss'
-import { gsap, Power4 } from 'gsap'
 
 const Project = () => {
-  const [translate, setTranslate] = React.useState(0)
-  const [status, setStatus] = React.useState(0)
-  const itemRef = React.useRef()
+  const refItem = React.useRef()
 
   React.useEffect(() => {
-    setStatus(-(100 * itemRef.current.offsetWidth) / window.innerWidth + 100)
-    const changeItemAnimation = () => {
-      gsap.to(itemRef.current, {
-        x: `${translate}vw`,
-        duration: 1,
-        ease: Power4.easeInOut
-      })
-    }
-    changeItemAnimation()
-  }, [translate])
-  
+
+  }, [])
 
   return (
     <>
-      <Container>
+      <div className={styles.containerProjects}>
         <section className={styles.projects}>
-          <button
-            className={styles.arrow}
-            onClick={() => {
-              if (translate === status) {
-                setTranslate(0)
-              } else {
-                setTranslate(translate - 100)
-              }
-            }}
-          >
-            <img src="arrow-pointer.svg" alt="Arrow" />
-          </button>
-          <Title text='Projetos' subTitle='Conheça meus projetos' />
-          <div ref={itemRef} className={styles.item}>
+          <Container>
+            <Title text='Projetos' subTitle='Conheça meus projetos' />
+          </Container>
+          <div className={styles.item}>
             <ProjectItem
               name='Breaking Bad'
               text='Este app consome uma api publica da série Braking Bad, basicamente ele lista todos os personagens da série e quando clicado ele mostra os atributos e de quais episódeos eles participaram.'
@@ -100,7 +78,7 @@ const Project = () => {
             />
           </div>
         </section>
-      </Container>
+      </div>
       <Preload text='Projetos' />
     </>
   )
