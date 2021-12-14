56162990206fba65ea6787cf505243fc0974ecb9
Hugo Ian <ahugaao@gmail.com>
2021-12-14 00:40:13 -0300

    fix bugs on pages

--- src/Components/Button/index.jsx
+++ src/Components/Button/index.jsx
@@ -2,16 +2,17 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import styles from './Button.module.scss'
 
-const Button = React.forwardRef((props, ref) => {
+const Button = ({ link, textOne, textTwo }) => {
   return (
     <div className={styles.button}>
-        <Link to={props.linkTo}>
-          <button ref={ref}>
-            <span>{props.text}</span>
+        <Link to={link}>
+          <button>
+            <span>{textOne}</span>
+            <span>{textTwo}</span>
           </button>
         </Link>
     </div>
   )
-})
+}
 
 export default Button
--- src/Components/ProjectItem/index.jsx
+++ src/Components/ProjectItem/index.jsx
@@ -1,32 +1,85 @@
 import React from 'react'
 import styles from './ProjectItem.module.scss'
+import { gsap } from 'gsap'
 
-const ProjectItem = ({ link, name, text, src, alt, techSrc }) => {
+const ProjectItem = ({ name, text, src, alt, techSrc }) => {
+  const titleAbout = React.useRef()
+  const textAbout = React.useRef()
+  const titleTech = React.useRef()
+  const techImgs = React.useRef()
+  const button = React.useRef()
+  const imgProject = React.useRef()
+
+  const project = React.useRef()
+
+  React.useEffect(() => {
+
+
+    const animaProject = () => {
+      gsap.to(titleAbout.current, {
+        x: 0,
+        duration: 0.5,
+        delay: 0.2
+      })
+      gsap.to(textAbout.current, {
+        x: 0,
+        duration: 0.4,
+        delay: 0.4
+      })
+      gsap.to(titleTech.current, {
+        x: 0,
+        duration: 0.5,
+        delay: 0.6
+      })
+      gsap.to(techImgs.current, {
+        x: 0,
+        duration: 0.5,
+        delay: 0.8
+      })
+      gsap.to(button.current, {
+        x: 0,
+        duration: 0.5,
+        delay: 1
+      })
+      gsap.to(imgProject.current, {
+        x: 0,
+        duration: 0.5,
+        delay: 1.2
+      })
+    }
+    
+    const observer = new IntersectionObserver((entries) => {
+      if (entries[0].isIntersecting) {
+        animaProject()
+      }
+    })
+
+    observer.observe(project.current)
+  }, [])
+
   return (
-    <a className={styles.link} href={link}>
-      <div className={styles.containerItem}>
-        <div className={styles.about}>
-          <h2>{name}</h2>
-          <p>{text}</p>
-          <div className={styles.tech}>
-            <span>Tecnologias</span>
-            <div className={styles.techSource}>
-              {techSrc.map((src, index) => {
-                return (
-                  <img key={index} src={src} alt={index} />
-                )
-              })}
-            </div>
+    <div className={styles.containerItem}>
+      <div ref={project} className={styles.about}>
+        <h2 ref={titleAbout}>{name}</h2>
+        <p ref={textAbout}>{text}</p>
+        <div className={styles.tech}>
+          <span ref={titleTech}>Tecnologias</span>
+          <div ref={techImgs} className={styles.techSource}>
+            {techSrc.map((src, index) => {
+              return (
+                <img key={index} src={src} alt={index} />
+              )
+            })}
           </div>
-          <button className={styles.button}>
-            Ver projeto
-          </button>
         </div>
-        <div className={styles.imgItem}>
-          <img src={src} alt={alt} />
-        </div>
+        <button ref={button} className={styles.button}>
+          Ver projeto
+        </button>
       </div>
-    </a>
+      <div className={styles.imgItem}>
+        <img ref={imgProject} src={src} alt={alt} />
+      </div>
+    </div>
   )
 }
 
