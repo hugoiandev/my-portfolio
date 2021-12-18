a115a272c01d3637c8897b07caef55ba08f3d94d
Hugo Ian <ahugaao@gmail.com>
2021-12-18 00:23:35 -0300

    new deploy

--- src/Utils/endpoint.js
+++ src/Utils/endpoint.js
@@ -5,3 +5,5 @@ export const GET_PROJECTS = () => {
     url: `https://my-portfolio2021.prismic.io/api/v2/documents/search?ref=${process.env.REACT_APP_REF}&access_token=${process.env.REACT_APP_ACCESS_TOKEN}`
   }
 }
+
+// new deploy
